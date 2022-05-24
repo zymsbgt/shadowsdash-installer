47b751d11c7731a9d1ca937c8e7779e52c6b1d0d
Zym <91821082+zymsbgt@users.noreply.github.com>
2022-05-24 21:07:00 +0800

    Update install-part-1.sh

--- install-part-1.sh
+++ install-part-1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Update and upgrade packages, to get up-to-date packages
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade
 # Restart the machine
 sudo reboot
