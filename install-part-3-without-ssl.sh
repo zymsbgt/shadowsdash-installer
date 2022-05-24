ab75b1e9462aeb5d7741fca5930a1b3b824fc543
Zym <91821082+zymsbgt@users.noreply.github.com>
2022-05-24 21:23:09 +0800

    bug fixes

--- install-part-3-without-ssl.sh
+++ install-part-3-without-ssl.sh
@@ -6,4 +6,6 @@ rm 000-default.conf
 # Get the new configuration
 wget https://raw.githubusercontent.com/zymsbgt/shadowsdash-installer/main/shadowsdash-nossl.conf
 # Open up the file with Nano for user to edit the <domain> field
-nano shadowsdash-nossl.conf
+#nano shadowsdash-nossl.conf
+# Tell the user to open the config with Nano
+echo "Type 'nano shadowsdash-nossl.conf' and replace the <domain> field with your own domain!"
--- install-part-4-without-ssl.sh
+++ install-part-4-without-ssl.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#cd /etc/apache2/sites-available
 sudo ln -s /etc/apache2/sites-available/shadowsdash.conf /etc/apache2/sites-enabled/shadowsdash.conf
 sudo a2enmod rewrite
 sudo systemctl restart apache2
